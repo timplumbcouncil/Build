e86ce8b30128218c1878744607d27eb3f1a43ad5
timplumbcouncil <TPlumb@carmarthenshire.gov.uk>
2015-12-09 10:48:51 +0000

    Add in header elements on mobile

--- js/custom.js
+++ js/custom.js
@@ -1,53 +1,61 @@
-
-
 $( document ).ready(function() {
 
-  populateSideNav()
+	populateSideNav()
 
-  $(".mobileNavToggle").focus(function() {
-    toggleMainMenu()
-  });
+	$(".mobileNavToggle").focus(function() {
+		openMainMenu()
+	});
+
+	$(".iconStack").hover(function() {
+		$(this).css('cursor','pointer');
+	}, function() {
+		$(this).css('cursor','auto');
+	});
+
+	$(".dropDownMenu").html($(".jumpBox").html())
 
 });
 
 $( window ).resize(function() {
-  if ($(".mobileMenu").is(":visible")) {console.log("now")}
+  if ($(".mobileNavToggle").is(":visible")) {
+
+  	//$(".logoRow").prepend($(".jumpBox").html())
+
+  } else {
+
+
+  }
      // console.log($( window ).width())
  });
 
 function populateSideNav() {
 
- $( ".sideNavUL li" ).each(function( index ) {
+	$( ".sideNavUL li" ).each(function( index ) {
+	  if ($(this).attr("class")) 
+	  {
+	    $(".sideMobileNavIcons").append("<div class='"+$(this).attr("class")+" iconStack' onclick='javascript:toggleSideMenu()''><span class='hiddenText'>Expand mobile menu</span></div>")
+	  }
+	})
 
-  if ($(this).attr("class")) 
-  {
-    $(".sideMobileNavIcons").append("<div class='"+$(this).attr("class")+" iconStack' onclick='javascript:toggleSideMenu()''><span class='hiddenText'>Expand mobile menu</span></div>")
-  }
+	$(".sideMobileNavList").html("<a class='closeMenu' href='javascript:toggleSideMenu()'><span class='hiddenText'>Close Menu</span></a><div class='sideNav'>" + $(".sideNav").html() + "</div>")
 
-})
+	$(".sideMobileNavIcons a:first").focus(function() {
+	  toggleSideMenu()
+	});
 
- $(".sideMobileNavList").html("<a class='closeMenu' href='javascript:toggleSideMenu()'><span class='hiddenText'>Close Menu</span></a><div class='sideNav'>" + $(".sideNav").html() + "</div>")
-
- $(".sideMobileNavIcons a:first").focus(function() {
-  toggleSideMenu()
-});
-
- $(".sideMobileNavList li a:last").blur(function() {
-  toggleSideMenu()
-});
-
+	$(".sideMobileNavList li a:last").blur(function() {
+	  toggleSideMenu()
+	});
 }
-
 
 function toggleSideMenu() {
 
- if ($(".sideMobileNavList").is(":visible")) {
-  closeSideNav()
-}
-else {
-  openSideNav()
-}
-
+	if ($(".sideMobileNavList").is(":visible")) {
+  		closeSideNav();
+	}
+	else {
+  		openSideNav();
+	}
 }
 
 function closeSideNav() {
@@ -61,14 +69,12 @@ function closeSideNav() {
 function openSideNav(){
 
   // count children
-
   var childElements = $('.active li').length
   var childPadding = childElements * 17
 
   if (childElements > 0) {
 
     // animte icons to match children
-
     $('.sideMobileNavIcons .active').animate({"padding-bottom": childPadding }, 500, function() { 
       $(".sideMobileNavList").fadeIn()
     })  
@@ -76,52 +82,44 @@ function openSideNav(){
   } else {
 
       // if no children, open without expanding
-
       $(".sideMobileNavList").fadeIn()
     }
+}
 
-  }
+function closeMainMenu() {
+	$(".mobileNavToggle").css('background-image', "url(images/main_nav_mobile.png)");  
+    $(".mobileNav a").attr("onclick","javascript:openMainMenu()");
+    $(".mobileMenu").fadeOut()
+}
 
-  function toggleMainMenu() {
-
-    if (!$(".mobileMenu").is(":visible")) {
-
+function openMainMenu(){
 
       // close top menu if side activated
 
       closeSideNav()
 
       // clear mobile menu
-
       $(".mobileMenu").html("<ul></ul>")
 
-      // add utility nav
+      // change graphic to close icon
+      $(".mobileNavToggle").css('background-image', "url(images/main_nav_mobile_close.png)");  
 
+      // add utility nav
       $( ".utility  li a" ).each(function( index ) {
         $(".mobileMenu ul").append("<li class='mobileUtilityNav'><a href='"+$(this).attr('href')+"'  >" + $( this ).text() + "</a></li>");
       });
 
       // add main nav
-
       $( ".mainNav  li a" ).each(function( index ) {
         $(".mobileMenu ul").append("<li class='mobileMainNav'><a href='"+$(this).attr('href')+"'  >" + $( this ).text() + "</a></li>");
       });
 
-      //$(".mobileMenu").append("<div class='mobileSearch'>" + $(".searchTool").html() + "</div>")
-      $(".mobileMenu").fadeIn( function() { $(".mobileNav a").attr("onclick","javascript:toggleMainMenu()");})
+      //$(".mobileNav").append("<div class='mobileSearch'>" + $(".searchTool").html() + "</div>")
+      $(".mobileMenu").fadeIn( function() { $(".mobileNav a").attr("onclick","javascript:closeMainMenu()");})
 
       // close main menu if tabbed out
 
       $(".mobileMenu a:last").blur(function() {
-        toggleMainMenu()
+        closeMainMenu()
       });
-
-    } else {
-
-      $(".mobileNav a").attr("onclick","javascript:toggleMainMenu()");
-
-      $(".mobileMenu").fadeOut()
-
-    }
-
-  }
+}
